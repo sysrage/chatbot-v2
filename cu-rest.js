ba8e0c3c73e42ebd5e16a5ca0c69b055674f68de
sysrage <sysrage@gmail.com>
2016-10-09 21:42:31 -0400

    .

--- cu-rest.js
+++ cu-rest.js
@@ -27,13 +27,13 @@ function restAPI(name) {
             host: "api.citystateentertainment.com"
         };
     }
-    
+
     function getServerURI(verb) {
         var host, port = 8000, protocol = "http:";
         switch (verb) {
             case "servers":
-                port = 8001;
-                host = getServerInfo().host;
+                port = 80;
+                host = "api.camelotunchained.com";
                 break;
             case "characters":
                 protocol = "https:";
@@ -47,10 +47,11 @@ function restAPI(name) {
         }
         return protocol + "//" + host + ":" + port + "/api/";
     }
-    
+
     var call = function(verb, params) {
         var serverURI = getServerURI(verb);
-    
+        if (verb === 'servers') serverURI = "https://api.camelotunchained.com/";
+
         // Call the CU REST API, returns a promise
         params = params || {};
         return new Promise(function (fulfill, reject) {
@@ -75,88 +76,83 @@ function restAPI(name) {
             });
         });
     };
-    
+
     return {
-    
+
         getAbilities: function() {
             return call("abilities", { timeout: 2000 });
         },
-    
+
         getAttributes: function() {
             return call("game/attributes", { timeout: 2000 });
         },
-        
+
         getBanes: function() {
             return call("game/banes", { timeout: 2000 });
         },
-        
+
         getBanners: function() {
             return call("banners", { timeout: 2000 });
         },
-        
+
         getBoons: function() {
             return call("game/boons", { timeout: 2000 });
         },
-        
+
         getBuildingBlocks: function() {
             return call("buildingblocks", {timeout: 2000 });
         },
-        
+
         getCharacters: function(loginToken) {
             return call("characters", { query: { loginToken: loginToken }, timeout: 2000 });
         },
-        
+
         getControlGame: function(query) {
             return call("game/controlgame", { query: query, timeout: 2000 });
         },
-        
+
         getCraftedAbilities: function(query) {
             return call("craftedabilities", { query: query, timeout: 2000 });
         },
-        
+
         getEvents: function() {
             return call("scheduledevents", { timeout: 2000 });
         },
-        
+
         getFactions: function() {
             return call("game/factions", { timeout: 2000 });
         },
-        
+
         getKills: function(query) {
             return call("kills", { query: query, timeout: 2000 });
         },
-        
+
         getPatchNotes: function() {
             return call("patchnotes", { timeout: 2000 });
         },
-        
+
         getPlayers: function() {
             return call("game/players", { timeout: 2000 });
         },
-        
+
         getRaces: function() {
             return call("game/races", { timeout: 2000 });
         },
-        
+
         getServers: function () {
             // Per CSE, there will be a single API for all online servers before we need
             // more than channel 4 and channel 10
             return new Promise(function (fulfill, reject) {
                 var serverList = [];
-                var channels = [
-                    call("servers", {query: {channelID:"4"}, timeout: 2000}),
-                    call("servers", {query: {channelID:"10"}, timeout: 2000}),
-                ];
-
-                Promise.all(channels).then(function (listAll) {
+                call("servers", {timeout: 2000}).then(function(listAll) {
                     for (var i = 0; i < listAll.length; i++) {
                         if (listAll[i] !== null) serverList = serverList.concat(listAll[i]);
                     }
                     fulfill(serverList);
-                })
+                });
             });
         },
-        
+
         getSpawnPoints: function() {
             return call("game/spawnpoints", {timeout: 2000 });
         }
